24a1b0507a50e3dda9c2ca26d690ca1112288306
hailegnaw27 <ixvos406@gmail.com>
2024-02-19 14:21:09 -0800

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,42 +1,24 @@
 #!/usr/bin/python3
-
+'''A script that gathers data from an API.
+'''
 import requests
-from sys import argv
+import sys
 
 
-def gather_data(employee_id):
-    """
-    Gathers data from the API for a given employee ID
-    and displays the employee's TODO list progress.
-    """
-    # Make a GET request to fetch employee information
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id)
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-    employee_name = user_data.get('name')
-
-    # Make a GET request to fetch employee's TODO list
-    todo_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id)
-    todo_response = requests.get(todo_url)
-    todo_data = todo_response.json()
-
-    # Calculate the number of completed tasks and total tasks
-    total_tasks = len(todo_data)
-    completed_tasks = sum(task.get('completed') for task in todo_data)
-
-    # Print the employee's TODO list progress
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, completed_tasks, total_tasks))
-
-    # Print the titles of completed tasks
-    for task in todo_data:
-        if task.get('completed'):
-            print("\t{}".format(task.get('title')))
+API_URL = 'https://jsonplaceholder.typicode.com'
+'''The API's URL.'''
 
 
-if __name__ == "__main__":
-    if len(argv) != 2 or not argv[1].isdigit():
-        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
-        exit(1)
-
-    employee_id = int(argv[1])
-    gather_data(employee_id)
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if sys.argv[1].isdigit():
+            employee_id = int(sys.argv[1])
+            user_response = requests.get(f'{API_URL}/users/{employee_id}').json()
+            todos_response = requests.get(f'{API_URL}/todos?userId={employee_id}').json()
+            employee_name = user_response.get('name')
+            total_tasks = len(todos_response)
+            completed_tasks = sum(task.get('completed') for task in todos_response)
+            print(f'Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):')
+            for task in todos_response:
+                if task.get('completed'):
+                    print(f'\t{task.get("title")}')
