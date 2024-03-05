68ef43b8ee8832c004a6a5713c16965e1ccfffb6
hailegnaw27 <ixvos406@gmail.com>
2024-03-05 07:42:10 -0800

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,9 +11,9 @@ def number_of_subscribers(subreddit):
         number of subscribers to the subreddit,
         or 0 if subreddit requested is invalid"""
     headers = {'User-Agent': 'xica369'}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        return (response.json().get("data").get("subscribers"))
-    return (0)
+        return response.json().get("data").get("subscribers")
+    return 0
