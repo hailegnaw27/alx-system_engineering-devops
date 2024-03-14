b87761117f654d4846b95639ba3f1a8e776e0d53
hailegnaw27 <ixvos406@gmail.com>
2024-03-14 05:19:20 -0700

    webstackdebugging

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,7 @@
+# handling multiple reqursts
+exec { 'fix--for-nginx':
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
+/etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
+
